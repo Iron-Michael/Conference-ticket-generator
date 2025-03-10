148b2f2c42b4983ad1ceb9af8d8acbf7b930277b
Sitthisak Logate <sitthisak@frevation.com>
2025-03-10 21:56:46 +0700

    set-up-desktop-bg-done-for-today

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MainBackground = () => {
+  return (
+  <div className="main-background">
+  <div className="pattern-lines"></div>
+  <div className="pattern-circle"></div>
+   <div className="pattern-circle-second"></div>
+  </div>
+
+  );
+};
+
+export default MainBackground;
---
+++ src/components/MainBackground.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainBackground = () => {
+  return (
+    <div className='main-background'>
+        
+    </div>
+  )
+}
+
+export default MainBackground
